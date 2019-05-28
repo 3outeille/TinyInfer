c1a1be08356faa4df327acd99db8c1c6a4453bad
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2019-05-28 23:23:45 +0800

    support cmake delta compilation

---
+++ infer/scripts/compile_pb.sh
@@ -0,0 +1,19 @@
+# Compile Probobuf
+# Input Params:
+#   1: compile command e.g. protoc / /usr/local/bin/protoc
+#   2: source directory
+#   3: target directory (for cmake delta compilation)
+#   4: built cache files (for protobuf recompile)
+
+# mkdir target directory
+mkdir -p $3
+mkdir -p $4
+
+# run compile command (compile to
+$1 -I=$2 --cpp_out=$4 $2/*
+
+# copy files changed from $4 to $3
+for fullpath in $4/*; do
+    f=$(echo $fullpath | sed "s/.*\///")
+    rsync --checksum $4/$f $3/$f
+done
