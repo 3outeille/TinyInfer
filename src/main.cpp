4267faa2ca3a2783b69b82827991d58f87c40f97
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2019-05-29 12:42:06 +0800

    Add proto definition

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/***
+ * Dummy File for testing cmake
+ * Includes some header files
+ */
+
+#include <iostream>
+#include "graph.pb.h"
+
+int main(int argc, const char * argv[]){
+    std::cout << "Hello World" << std::endl;
+    tensorflow::GraphDef graph_def;
+
+    return 0;
+}
