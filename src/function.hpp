8882c1bbfa9b17daced08984393ac02effc04b17
Zhiqiang Xie <xiezhq@shanghaitech.edu.cn>
2019-05-30 03:28:25 +0800

    setup simple function

--- src/function.hpp
+++ src/function.hpp
@@ -4,5 +4,16 @@
 #include "op/parameter.hpp"
 
 namespace tinyinfer {
-class Function {};
+class Function {
+ protected:
+  NodeVector m_graph;
+  ParameterVector m_parameters;
+
+ public:
+  Function(const NodeVector& graph, const ParameterVector& parameters);
+  ~Function() {}
+
+  // passing by runtime::Tensor here might be the most convinient interface
+  void forward(const std::vector<runtime::Tensor> inputs);
+};
 }
