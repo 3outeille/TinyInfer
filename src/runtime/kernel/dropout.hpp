c90deca3b6e99b8895ac1e79ba7bc684c54c1ed4
jdtsui <jdtsui@outlook.com>
2019-05-30 02:18:04 +0800

    kernel op

--- src/runtime/kernel/dropout.hpp
+++ src/runtime/kernel/dropout.hpp
@@ -7,29 +7,26 @@
 
 #include "runtime/tensor.hpp"
 
-typedef typename Eigen::Tensor<float, 1> Tensor1f;
-typedef typename Eigen::Tensor<float, 2> Tensor2f;
-typedef typename Eigen::Tensor<float, 3> Tensor3f;
-typedef typename Eigen::Tensor<float, 4> Tensor4f;
-
 namespace tinyinfer {
 namespace runtime {
 namespace kernel {
+
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor> Tensor1f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor> Tensor2f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor> Tensor3f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor> Tensor4f;
 
 /**
  * @brief drop out layer
  * @param _input
  */
-Tensor2f Dropout(Tensor2f _input) {
-    return _input;
-}
-
-Tensor3f Dropout(Tensor3f _input) {
-    return _input;
-}
-
-Tensor4f Dropout(Tensor4f _input) {
-    return _input;
+std::shared_ptr<runtime::Tensor> Dropout(std::shared_ptr<runtime::Tensor> input) {
+    if (input->get_rank() == 2)
+        return std::make_shared<runtime::Tensor>(input->get_tensor_r2_ptr());
+    else if (input->get_rank() == 3)
+        return std::make_shared<runtime::Tensor>(input->get_tensor_r3_ptr());
+    else if (input->get_rank() == 4)
+        return std::make_shared<runtime::Tensor>(input->get_tensor_r4_ptr());
 }
 }
 }
--- src/runtime/kernel/relu.hpp
+++ src/runtime/kernel/relu.hpp
@@ -7,29 +7,32 @@
 
 #include "runtime/tensor.hpp"
 
-typedef typename Eigen::Tensor<float, 1> Tensor1f;
-typedef typename Eigen::Tensor<float, 2> Tensor2f;
-typedef typename Eigen::Tensor<float, 3> Tensor3f;
-typedef typename Eigen::Tensor<float, 4> Tensor4f;
-
 namespace tinyinfer {
 namespace runtime {
 namespace kernel {
+
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor> Tensor1f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor> Tensor2f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor> Tensor3f;
+typedef typename Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor> Tensor4f;
 
 /**
  * @brief relu layer
  * @param _input
  */
-Tensor2f Relu(Tensor2f _input) {
-    return _input.cwiseMax(static_cast<float>(0));
-}
+std::shared_ptr<runtime::Tensor> Relu(std::shared_ptr<runtime::Tensor> input) {
 
-Tensor3f Relu(Tensor3f _input) {
-    return _input.cwiseMax(static_cast<float>(0));
-}
+    if (input->get_rank() == 2) {
+        Tensor2f _input = input->get_tensor_r2_ptr();
+        return std::make_shared<runtime::Tensor>(_input.cwiseMax(static_cast<float>(0)));
+    } else if (input->get_rank() == 3) {
+        Tensor3f _input = input->get_tensor_r3_ptr();
+        return std::make_shared<runtime::Tensor>(_input.cwiseMax(static_cast<float>(0)));
+    }else if (input->get_rank() == 4) {
+        Tensor4f _input = input->get_tensor_r4_ptr();
+        return std::make_shared<runtime::Tensor>(_input.cwiseMax(static_cast<float>(0)));
+    }
 
-Tensor4f Relu(Tensor4f _input) {
-    return _input.cwiseMax(static_cast<float>(0));
 }
 }
 }
