878f91f969860811a48afe1492b5e6eaaa42a909
Zhiqiang Xie <xiezhq@shanghaitech.edu.cn>
2019-05-29 10:25:02 +0800

    input/output, tensor setup

---
+++ src/output.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <set>
+
+#include "input.hpp"
+#include "runtime/tensor.hpp"
+
+namespace tinyinfer {
+class Node;
+
+class Output {
+ protected:
+  Node* m_node;
+  size_t m_index;
+  std::shared_ptr<runtime::Tensor> m_tensor;
+  std::set<Input*> m_inputs;
+
+ public:
+  Output(Node* node, size_t index,
+         const std::shared_ptr<runtime::Tensor>& tensor);
+
+  std::shared_ptr<Node> get_node() const;
+  size_t get_index() const { return m_index; }
+  std::shared_ptr<runtime::Tensor> get_tensor_ptr() const { return m_tensor; }
+  void set_tensor_ptr(const std::shared_ptr<runtime::Tensor>& tensor);
+
+  void add_input(Input* input);
+  void remove_input(Input* input);
+  
+  const std::set<Input*>& get_inputs() const { return m_inputs; }
+  const Shape& get_shape() const;
+
+ private:
+  Output(const Output&) = delete;
+  Output(Output&&) = delete;
+  Output& operator=(const Output&) = delete;
+};
+}
