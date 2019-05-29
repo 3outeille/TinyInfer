8c79a18196f0052ea3b2f0a86e0c709decdfe653
Zhiqiang Xie <xiezhq@shanghaitech.edu.cn>
2019-05-30 00:39:59 +0800

    simplify the tensor definition

--- src/runtime/tensor.hpp
+++ src/runtime/tensor.hpp
@@ -14,31 +14,35 @@ class Tensor {
  private:
   Shape m_shape;
   size_t m_rank;
-  std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor>>
-      m_tensor_rank1;
-  std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor>>
-      m_tensor_rank2;
-  std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor>>
-      m_tensor_rank3;
-  std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor>>
-      m_tensor_rank4;
+  Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor> m_tensor_rank1;
+  Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor> m_tensor_rank2;
+  Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor> m_tensor_rank3;
+  Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor> m_tensor_rank4;
 
  public:
-  // Todo: initialize the m_shape while constructing
-  Tensor(std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor>>
-             tensor);
-  Tensor(std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor>>
-             tensor);
-  Tensor(std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor>>
-             tensor);
-  Tensor(std::unique_ptr<Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor>>
-             tensor);
-  //   Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor>&& tensor);
-  //   Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor>&& tensor);
-  //   Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor>&& tensor);
-  //   Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor>&& tensor);
+  Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor>& tensor);
+  Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor>& tensor);
+  Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor>& tensor);
+  Tensor(Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor>& tensor);
 
   const Shape& get_shape() const { return m_shape; }
+
+  const Eigen::Tensor<TENSOR_DATA_TYPE, 1, Eigen::RowMajor>&
+  get_tensor_r1_ptr() {
+    return m_tensor_rank1;
+  }
+  const Eigen::Tensor<TENSOR_DATA_TYPE, 2, Eigen::RowMajor>&
+  get_tensor_r2_ptr() {
+    return m_tensor_rank2;
+  }
+  const Eigen::Tensor<TENSOR_DATA_TYPE, 3, Eigen::RowMajor>&
+  get_tensor_r3_ptr() {
+    return m_tensor_rank3;
+  }
+  const Eigen::Tensor<TENSOR_DATA_TYPE, 4, Eigen::RowMajor>&
+  get_tensor_r4_ptr() {
+    return m_tensor_rank4;
+  }
 };
 }  // namespace runtime
 }  // namespace tinyinfer
